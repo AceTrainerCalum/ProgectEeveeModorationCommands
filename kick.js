6b2761ed6307d0bd586356da9c979c2365bdcdbf
AceTrainerCalum <79257331+AceTrainerCalum@users.noreply.github.com>
2021-04-09 11:30:03 +0100

    Add files via upload

---
+++ kick.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'kick',
+    description: "this is a kick command",
+    async execute(Client, message, args, discord) {
+        const member = message.mentions.users.first();
+        if (message.member.permissions.has("KICK_MEMBERS")) {
+            if (member) {
+                const memberTarget = message.guild.members.cache.get(member.id);
+                message.channel.send(`${message.author} kicked a member `);
+                memberTarget.kick();
+                message.channel.send("User has been kicked");
+                console.log(`${message.author.id} kicked a member!`)
+                console.log(` `)
+                console.log(` --- new accounnt of punnishment ---`)
+                console.log(` `)
+            } else {
+                message.channel.send(`${message.author} You can NOT kick memmbers`);
+                message.channel.send(`if you think this is a mistake contact administrators or cheack if your roles have kick members permition!`);
+            }
+        }
+
+    }
+}
